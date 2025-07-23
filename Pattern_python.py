679bf318ed504f98a8ccb73845a3c440e313e912
Gaurav Gautam <gauravgautam9024@gmail.com>
2025-07-23 14:10:31 +0530

    Update Pattern_python.py

--- Pattern_python.py
+++ Pattern_python.py
@@ -140,3 +140,60 @@ while(i<=5):
  *  * 
  * 
 """
+
+#pattern 
+i = 1
+while(i<=4):
+    j =1
+    while(j<=4):
+        if(j==2 or j==3) and (i==2 or i ==3):
+         print(" ",end="")
+        else:
+           print("*",end="")
+        j+=1
+    print()
+    i+=1
+""" output-:
+ ****
+ *  *
+ *  *
+ ****   """
+
+#program
+i = 1
+while(i<=4):
+    j =1
+    while(j<=5):
+        if(j==2 or j ==4 ) and (i==2) or (j==2 or j==3 or j==4) and (i==3) or (j==2 or j==3 or j==4) and (i==4):
+         print(" ",end="")
+        else:
+           print("*",end="")
+        j+=1
+    print()
+    i+=1
+    """
+output:-
+*****
+* * *
+*   *
+*   *
+""" 
+
+#program
+i = 1
+while(i<=4):
+    j =1
+    while(j<=4):
+        if(j ==1 or j==2 or j ==3 or j==5) and (i==2) or ( j==3 or j ==4 or j==5 or j==1 ) and (i==3):
+         print(" ",end="")
+        else:
+           print("*",end="")
+        j+=1
+    print()
+    i+=1
+"""
+output:-
+****
+   *
+ *
+****"""
